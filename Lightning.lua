45f646290b1f59ff0e677682e5ba7d08024a8272
Zixsoed <rjjdksksosos@gmail.com>
2024-08-26 17:58:31 +0700

    Create Lightning.lua

---
+++ Lightning.lua
@@ -0,0 +1,391 @@
+local Creator = loadstring(game:HttpGet("https://glot.io/snippets/gxfazuhbjw/raw"))()
+
+Creator.lights({
+    Lightid = "rbxassetid://13368694976",
+    LightRoomDestroy = 101 --u can set it after 51 or any door num you want
+})
+game.ReplicatedStorage.GameData.LatestRoom.Changed:Wait()
+local Creator = loadstring(game:HttpGet("https://glot.io/snippets/gxfazuhbjw/raw"))()
+
+Creator.lights({
+    Lightid = "rbxassetid://13368694976",
+    LightRoomDestroy = 101 --u can set it after 51 or any door num you want
+})
+game.ReplicatedStorage.GameData.LatestRoom.Changed:Wait()
+local Creator = loadstring(game:HttpGet("https://glot.io/snippets/gxfazuhbjw/raw"))()
+
+Creator.lights({
+    Lightid = "rbxassetid://13368694976",
+    LightRoomDestroy = 101 --u can set it after 51 or any door num you want
+})
+game.ReplicatedStorage.GameData.LatestRoom.Changed:Wait()
+local Creator = loadstring(game:HttpGet("https://glot.io/snippets/gxfazuhbjw/raw"))()
+
+Creator.lights({
+    Lightid = "rbxassetid://13368694976",
+    LightRoomDestroy = 101 --u can set it after 51 or any door num you want
+})
+game.ReplicatedStorage.GameData.LatestRoom.Changed:Wait()
+local Creator = loadstring(game:HttpGet("https://glot.io/snippets/gxfazuhbjw/raw"))()
+
+Creator.lights({
+    Lightid = "rbxassetid://13368694976",
+    LightRoomDestroy = 101 --u can set it after 51 or any door num you want
+})
+game.ReplicatedStorage.GameData.LatestRoom.Changed:Wait()
+local Creator = loadstring(game:HttpGet("https://glot.io/snippets/gxfazuhbjw/raw"))()
+
+Creator.lights({
+    Lightid = "rbxassetid://13368694976",
+    LightRoomDestroy = 101 --u can set it after 51 or any door num you want
+})
+game.ReplicatedStorage.GameData.LatestRoom.Changed:Wait()
+local Creator = loadstring(game:HttpGet("https://glot.io/snippets/gxfazuhbjw/raw"))()
+
+Creator.lights({
+    Lightid = "rbxassetid://13368694976",
+    LightRoomDestroy = 101 --u can set it after 51 or any door num you want
+})
+game.ReplicatedStorage.GameData.LatestRoom.Changed:Wait()
+local Creator = loadstring(game:HttpGet("https://glot.io/snippets/gxfazuhbjw/raw"))()
+
+Creator.lights({
+    Lightid = "rbxassetid://13368694976",
+    LightRoomDestroy = 101 --u can set it after 51 or any door num you want
+})
+game.ReplicatedStorage.GameData.LatestRoom.Changed:Wait()
+local Creator = loadstring(game:HttpGet("https://glot.io/snippets/gxfazuhbjw/raw"))()
+
+Creator.lights({
+    Lightid = "rbxassetid://13368694976",
+    LightRoomDestroy = 101 --u can set it after 51 or any door num you want
+})
+game.ReplicatedStorage.GameData.LatestRoom.Changed:Wait()
+local Creator = loadstring(game:HttpGet("https://glot.io/snippets/gxfazuhbjw/raw"))()
+
+Creator.lights({
+    Lightid = "rbxassetid://13368694976",
+    LightRoomDestroy = 101 --u can set it after 51 or any door num you want
+})
+game.ReplicatedStorage.GameData.LatestRoom.Changed:Wait()
+local Creator = loadstring(game:HttpGet("https://glot.io/snippets/gxfazuhbjw/raw"))()
+
+Creator.lights({
+    Lightid = "rbxassetid://13368694976",
+    LightRoomDestroy = 101 --u can set it after 51 or any door num you want
+})
+game.ReplicatedStorage.GameData.LatestRoom.Changed:Wait()
+local Creator = loadstring(game:HttpGet("https://glot.io/snippets/gxfazuhbjw/raw"))()
+
+Creator.lights({
+    Lightid = "rbxassetid://13368694976",
+    LightRoomDestroy = 101 --u can set it after 51 or any door num you want
+})
+game.ReplicatedStorage.GameData.LatestRoom.Changed:Wait()
+local Creator = loadstring(game:HttpGet("https://glot.io/snippets/gxfazuhbjw/raw"))()
+
+Creator.lights({
+    Lightid = "rbxassetid://13368694976",
+    LightRoomDestroy = 101 --u can set it after 51 or any door num you want
+})
+game.ReplicatedStorage.GameData.LatestRoom.Changed:Wait()
+local Creator = loadstring(game:HttpGet("https://glot.io/snippets/gxfazuhbjw/raw"))()
+
+Creator.lights({
+    Lightid = "rbxassetid://13368694976",
+    LightRoomDestroy = 101 --u can set it after 51 or any door num you want
+})
+game.ReplicatedStorage.GameData.LatestRoom.Changed:Wait()
+local Creator = loadstring(game:HttpGet("https://glot.io/snippets/gxfazuhbjw/raw"))()
+
+Creator.lights({
+    Lightid = "rbxassetid://13368694976",
+    LightRoomDestroy = 101 --u can set it after 51 or any door num you want
+})
+game.ReplicatedStorage.GameData.LatestRoom.Changed:Wait()
+local Creator = loadstring(game:HttpGet("https://glot.io/snippets/gxfazuhbjw/raw"))()
+
+Creator.lights({
+    Lightid = "rbxassetid://13368694976",
+    LightRoomDestroy = 101 --u can set it after 51 or any door num you want
+})
+game.ReplicatedStorage.GameData.LatestRoom.Changed:Wait()
+local Creator = loadstring(game:HttpGet("https://glot.io/snippets/gxfazuhbjw/raw"))()
+
+Creator.lights({
+    Lightid = "rbxassetid://13368694976",
+    LightRoomDestroy = 101 --u can set it after 51 or any door num you want
+})
+game.ReplicatedStorage.GameData.LatestRoom.Changed:Wait()
+local Creator = loadstring(game:HttpGet("https://glot.io/snippets/gxfazuhbjw/raw"))()
+
+Creator.lights({
+    Lightid = "rbxassetid://13368694976",
+    LightRoomDestroy = 101 --u can set it after 51 or any door num you want
+})
+game.ReplicatedStorage.GameData.LatestRoom.Changed:Wait()
+local Creator = loadstring(game:HttpGet("https://glot.io/snippets/gxfazuhbjw/raw"))()
+
+Creator.lights({
+    Lightid = "rbxassetid://13368694976",
+    LightRoomDestroy = 101 --u can set it after 51 or any door num you want
+})
+game.ReplicatedStorage.GameData.LatestRoom.Changed:Wait()
+local Creator = loadstring(game:HttpGet("https://glot.io/snippets/gxfazuhbjw/raw"))()
+
+Creator.lights({
+    Lightid = "rbxassetid://13368694976",
+    LightRoomDestroy = 101 --u can set it after 51 or any door num you want
+})
+game.ReplicatedStorage.GameData.LatestRoom.Changed:Wait()
+local Creator = loadstring(game:HttpGet("https://glot.io/snippets/gxfazuhbjw/raw"))()
+
+Creator.lights({
+    Lightid = "rbxassetid://13368694976",
+    LightRoomDestroy = 101 --u can set it after 51 or any door num you want
+})
+game.ReplicatedStorage.GameData.LatestRoom.Changed:Wait()
+local Creator = loadstring(game:HttpGet("https://glot.io/snippets/gxfazuhbjw/raw"))()
+
+Creator.lights({
+    Lightid = "rbxassetid://13368694976",
+    LightRoomDestroy = 101 --u can set it after 51 or any door num you want
+})
+game.ReplicatedStorage.GameData.LatestRoom.Changed:Wait()
+local Creator = loadstring(game:HttpGet("https://glot.io/snippets/gxfazuhbjw/raw"))()
+
+Creator.lights({
+    Lightid = "rbxassetid://13368694976",
+    LightRoomDestroy = 101 --u can set it after 51 or any door num you want
+})
+game.ReplicatedStorage.GameData.LatestRoom.Changed:Wait()
+local Creator = loadstring(game:HttpGet("https://glot.io/snippets/gxfazuhbjw/raw"))()
+
+Creator.lights({
+    Lightid = "rbxassetid://13368694976",
+    LightRoomDestroy = 101 --u can set it after 51 or any door num you want
+})
+game.ReplicatedStorage.GameData.LatestRoom.Changed:Wait()
+local Creator = loadstring(game:HttpGet("https://glot.io/snippets/gxfazuhbjw/raw"))()
+
+Creator.lights({
+    Lightid = "rbxassetid://13368694976",
+    LightRoomDestroy = 101 --u can set it after 51 or any door num you want
+})
+game.ReplicatedStorage.GameData.LatestRoom.Changed:Wait()
+local Creator = loadstring(game:HttpGet("https://glot.io/snippets/gxfazuhbjw/raw"))()
+
+Creator.lights({
+    Lightid = "rbxassetid://13368694976",
+    LightRoomDestroy = 101 --u can set it after 51 or any door num you want
+})
+game.ReplicatedStorage.GameData.LatestRoom.Changed:Wait()
+local Creator = loadstring(game:HttpGet("https://glot.io/snippets/gxfazuhbjw/raw"))()
+
+Creator.lights({
+    Lightid = "rbxassetid://13368694976",
+    LightRoomDestroy = 101 --u can set it after 51 or any door num you want
+})
+game.ReplicatedStorage.GameData.LatestRoom.Changed:Wait()
+local Creator = loadstring(game:HttpGet("https://glot.io/snippets/gxfazuhbjw/raw"))()
+
+Creator.lights({
+    Lightid = "rbxassetid://13368694976",
+    LightRoomDestroy = 101 --u can set it after 51 or any door num you want
+})
+game.ReplicatedStorage.GameData.LatestRoom.Changed:Wait()
+local Creator = loadstring(game:HttpGet("https://glot.io/snippets/gxfazuhbjw/raw"))()
+
+Creator.lights({
+    Lightid = "rbxassetid://13368694976",
+    LightRoomDestroy = 101 --u can set it after 51 or any door num you want
+})
+game.ReplicatedStorage.GameData.LatestRoom.Changed:Wait()
+local Creator = loadstring(game:HttpGet("https://glot.io/snippets/gxfazuhbjw/raw"))()
+
+Creator.lights({
+    Lightid = "rbxassetid://13368694976",
+    LightRoomDestroy = 101 --u can set it after 51 or any door num you want
+})
+game.ReplicatedStorage.GameData.LatestRoom.Changed:Wait()
+local Creator = loadstring(game:HttpGet("https://glot.io/snippets/gxfazuhbjw/raw"))()
+
+Creator.lights({
+    Lightid = "rbxassetid://13368694976",
+    LightRoomDestroy = 101 --u can set it after 51 or any door num you want
+})
+game.ReplicatedStorage.GameData.LatestRoom.Changed:Wait()
+local Creator = loadstring(game:HttpGet("https://glot.io/snippets/gxfazuhbjw/raw"))()
+
+Creator.lights({
+    Lightid = "rbxassetid://13368694976",
+    LightRoomDestroy = 101 --u can set it after 51 or any door num you want
+})
+game.ReplicatedStorage.GameData.LatestRoom.Changed:Wait()
+local Creator = loadstring(game:HttpGet("https://glot.io/snippets/gxfazuhbjw/raw"))()
+
+Creator.lights({
+    Lightid = "rbxassetid://13368694976",
+    LightRoomDestroy = 101 --u can set it after 51 or any door num you want
+})
+game.ReplicatedStorage.GameData.LatestRoom.Changed:Wait()
+local Creator = loadstring(game:HttpGet("https://glot.io/snippets/gxfazuhbjw/raw"))()
+
+Creator.lights({
+    Lightid = "rbxassetid://13368694976",
+    LightRoomDestroy = 101 --u can set it after 51 or any door num you want
+})
+game.ReplicatedStorage.GameData.LatestRoom.Changed:Wait()
+local Creator = loadstring(game:HttpGet("https://glot.io/snippets/gxfazuhbjw/raw"))()
+
+Creator.lights({
+    Lightid = "rbxassetid://13368694976",
+    LightRoomDestroy = 101 --u can set it after 51 or any door num you want
+})
+game.ReplicatedStorage.GameData.LatestRoom.Changed:Wait()
+local Creator = loadstring(game:HttpGet("https://glot.io/snippets/gxfazuhbjw/raw"))()
+
+Creator.lights({
+    Lightid = "rbxassetid://13368694976",
+    LightRoomDestroy = 101 --u can set it after 51 or any door num you want
+})
+game.ReplicatedStorage.GameData.LatestRoom.Changed:Wait()
+local Creator = loadstring(game:HttpGet("https://glot.io/snippets/gxfazuhbjw/raw"))()
+
+Creator.lights({
+    Lightid = "rbxassetid://13368694976",
+    LightRoomDestroy = 101 --u can set it after 51 or any door num you want
+})
+game.ReplicatedStorage.GameData.LatestRoom.Changed:Wait()
+local Creator = loadstring(game:HttpGet("https://glot.io/snippets/gxfazuhbjw/raw"))()
+
+Creator.lights({
+    Lightid = "rbxassetid://13368694976",
+    LightRoomDestroy = 101 --u can set it after 51 or any door num you want
+})
+game.ReplicatedStorage.GameData.LatestRoom.Changed:Wait()
+local Creator = loadstring(game:HttpGet("https://glot.io/snippets/gxfazuhbjw/raw"))()
+
+Creator.lights({
+    Lightid = "rbxassetid://13368694976",
+    LightRoomDestroy = 101 --u can set it after 51 or any door num you want
+})
+game.ReplicatedStorage.GameData.LatestRoom.Changed:Wait()
+local Creator = loadstring(game:HttpGet("https://glot.io/snippets/gxfazuhbjw/raw"))()
+
+Creator.lights({
+    Lightid = "rbxassetid://13368694976",
+    LightRoomDestroy = 101 --u can set it after 51 or any door num you want
+})
+game.ReplicatedStorage.GameData.LatestRoom.Changed:Wait()
+local Creator = loadstring(game:HttpGet("https://glot.io/snippets/gxfazuhbjw/raw"))()
+
+Creator.lights({
+    Lightid = "rbxassetid://13368694976",
+    LightRoomDestroy = 101 --u can set it after 51 or any door num you want
+})
+game.ReplicatedStorage.GameData.LatestRoom.Changed:Wait()
+local Creator = loadstring(game:HttpGet("https://glot.io/snippets/gxfazuhbjw/raw"))()
+
+Creator.lights({
+    Lightid = "rbxassetid://13368694976",
+    LightRoomDestroy = 101 --u can set it after 51 or any door num you want
+})
+game.ReplicatedStorage.GameData.LatestRoom.Changed:Wait()
+local Creator = loadstring(game:HttpGet("https://glot.io/snippets/gxfazuhbjw/raw"))()
+
+Creator.lights({
+    Lightid = "rbxassetid://13368694976",
+    LightRoomDestroy = 101 --u can set it after 51 or any door num you want
+})
+game.ReplicatedStorage.GameData.LatestRoom.Changed:Wait()
+local Creator = loadstring(game:HttpGet("https://glot.io/snippets/gxfazuhbjw/raw"))()
+
+Creator.lights({
+    Lightid = "rbxassetid://13368694976",
+    LightRoomDestroy = 101 --u can set it after 51 or any door num you want
+})
+game.ReplicatedStorage.GameData.LatestRoom.Changed:Wait()
+local Creator = loadstring(game:HttpGet("https://glot.io/snippets/gxfazuhbjw/raw"))()
+
+Creator.lights({
+    Lightid = "rbxassetid://13368694976",
+    LightRoomDestroy = 101 --u can set it after 51 or any door num you want
+})
+game.ReplicatedStorage.GameData.LatestRoom.Changed:Wait()
+local Creator = loadstring(game:HttpGet("https://glot.io/snippets/gxfazuhbjw/raw"))()
+
+Creator.lights({
+    Lightid = "rbxassetid://13368694976",
+    LightRoomDestroy = 101 --u can set it after 51 or any door num you want
+})
+game.ReplicatedStorage.GameData.LatestRoom.Changed:Wait()
+local Creator = loadstring(game:HttpGet("https://glot.io/snippets/gxfazuhbjw/raw"))()
+
+Creator.lights({
+    Lightid = "rbxassetid://13368694976",
+    LightRoomDestroy = 101 --u can set it after 51 or any door num you want
+})
+game.ReplicatedStorage.GameData.LatestRoom.Changed:Wait()
+local Creator = loadstring(game:HttpGet("https://glot.io/snippets/gxfazuhbjw/raw"))()
+
+Creator.lights({
+    Lightid = "rbxassetid://13368694976",
+    LightRoomDestroy = 101 --u can set it after 51 or any door num you want
+})
+game.ReplicatedStorage.GameData.LatestRoom.Changed:Wait()
+local Creator = loadstring(game:HttpGet("https://glot.io/snippets/gxfazuhbjw/raw"))()
+
+Creator.lights({
+    Lightid = "rbxassetid://13368694976",
+    LightRoomDestroy = 101 --u can set it after 51 or any door num you want
+})
+game.ReplicatedStorage.GameData.LatestRoom.Changed:Wait()
+local Creator = loadstring(game:HttpGet("https://glot.io/snippets/gxfazuhbjw/raw"))()
+
+Creator.lights({
+    Lightid = "rbxassetid://13368694976",
+    LightRoomDestroy = 101 --u can set it after 51 or any door num you want
+})
+game.ReplicatedStorage.GameData.LatestRoom.Changed:Wait()
+local Creator = loadstring(game:HttpGet("https://glot.io/snippets/gxfazuhbjw/raw"))()
+
+Creator.lights({
+    Lightid = "rbxassetid://13368694976",
+    LightRoomDestroy = 101 --u can set it after 51 or any door num you want
+})
+game.ReplicatedStorage.GameData.LatestRoom.Changed:Wait()
+local Creator = loadstring(game:HttpGet("https://glot.io/snippets/gxfazuhbjw/raw"))()
+
+Creator.lights({
+    Lightid = "rbxassetid://13368694976",
+    LightRoomDestroy = 101 --u can set it after 51 or any door num you want
+})
+game.ReplicatedStorage.GameData.LatestRoom.Changed:Wait()
+local Creator = loadstring(game:HttpGet("https://glot.io/snippets/gxfazuhbjw/raw"))()
+
+Creator.lights({
+    Lightid = "rbxassetid://13368694976",
+    LightRoomDestroy = 101 --u can set it after 51 or any door num you want
+})
+game.ReplicatedStorage.GameData.LatestRoom.Changed:Wait()
+local Creator = loadstring(game:HttpGet("https://glot.io/snippets/gxfazuhbjw/raw"))()
+
+Creator.lights({
+    Lightid = "rbxassetid://13368694976",
+    LightRoomDestroy = 101 --u can set it after 51 or any door num you want
+})
+game.ReplicatedStorage.GameData.LatestRoom.Changed:Wait()
+local Creator = loadstring(game:HttpGet("https://glot.io/snippets/gxfazuhbjw/raw"))()
+
+Creator.lights({
+    Lightid = "rbxassetid://13368694976",
+    LightRoomDestroy = 101 --u can set it after 51 or any door num you want
+})
+game.ReplicatedStorage.GameData.LatestRoom.Changed:Wait()
+local Creator = loadstring(game:HttpGet("https://glot.io/snippets/gxfazuhbjw/raw"))()
+
+Creator.lights({
+    Lightid = "rbxassetid://13368694976",
+    LightRoomDestroy = 101 --u can set it after 51 or any door num you want
+})
